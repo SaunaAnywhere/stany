0c50577fbcd6546e34074708a842e1a858c557aa
Matej Sajgal <Matej_Sajgal@swissre.com>
2022-02-15 20:54:10 +0100

    Adds Home page + prettier

---
+++ lib/content.ts
@@ -0,0 +1,32 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { remark } from "remark";
+import html from "remark-html";
+
+const contentDirectory = path.join(process.cwd(), "content");
+
+export async function getContentData(fileName: string) {
+  const fullPath = path.join(contentDirectory, `${fileName}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+
+  // Use remark to convert markdown into HTML string
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content);
+  const contentHtml = processedContent.toString();
+
+  // Combine the data with the id and contentHtml
+  return {
+    fileName,
+    contentHtml,
+    ...(matterResult.data as {
+      date: string;
+      title: string;
+      cats: { description: string; name: string }[];
+    }),
+  };
+}
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,28 +1,44 @@
-import Head from "next/head"
-import { Component } from 'react'
-import { attributes, react as HomeContent } from '../content/home.md';
+import Script from "next/script";
+import { GetStaticProps } from "next";
 
-export default class Home extends Component {
-  render() {
-    let { title, cats } = attributes;
-    return (
-      <>
-        <Head>
-          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-        </Head>
-        <article>
-          <h1>{title}</h1>
-          <HomeContent />
-          <ul>
-            {cats.map((cat, k) => (
-              <li key={k}>
-                <h2>{cat.name}</h2>
-                <p>{cat.description}</p>
-              </li>
-            ))}
-          </ul>
-        </article>
-      </>
-    )
-  }
+import { getContentData } from "../lib/content";
+
+export default function Home({
+  contentData,
+}: {
+  contentData: {
+    title: string;
+    date: string;
+    cats: { name: string; description: string }[];
+    contentHtml: string;
+  };
+}) {
+  return (
+    <>
+      <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></Script>
+      <article>
+        <h1>{contentData.title}</h1>
+        <section
+          dangerouslySetInnerHTML={{ __html: contentData.contentHtml }}
+        />
+        <ul>
+          {contentData.cats.map((cat, k) => (
+            <li key={k}>
+              <h2>{cat.name}</h2>
+              <p>{cat.description}</p>
+            </li>
+          ))}
+        </ul>
+      </article>
+    </>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const contentData = await getContentData("home");
+  return {
+    props: {
+      contentData,
+    },
+  };
+};
