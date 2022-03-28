d20a0895cd96636ba447705f4383d9ed9ae5869d
Matej Sajgal <Matej_Sajgal@swissre.com>
2022-03-28 22:08:58 +0200

    Adds Option Box

--- components/Options.tsx
+++ components/Options.tsx
@@ -1,5 +1,75 @@
-import { Box } from "@mui/material";
+import styled from "@emotion/styled";
+import { Box, Divider, Typography } from "@mui/material";
+import WorkspacePremiumIcon from "@mui/icons-material/WorkspacePremium";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import QuickreplyIcon from "@mui/icons-material/Quickreply";
+
+const StyledBox = styled(Box)`
+  display: flex;
+  justify-content: center;
+`;
+
+const Option = styled(Box)`
+  /* width: 20em; */
+  padding: 2em;
+  display: flex;
+  justify-content: center;
+`;
+
+const IconBox = styled(Box)`
+  padding: 1em;
+`;
+
+const OptionContent = styled(Box)``;
 
 export default function Options() {
-  return <Box>Option 1 | Option 2 | Option 3</Box>;
+  return (
+    <StyledBox>
+      <Option>
+        <IconBox>
+          <WorkspacePremiumIcon />
+        </IconBox>
+        <OptionContent>
+          <Typography variant="h5" fontWeight="500" component="div">
+            Garancia spokojnosti
+          </Typography>
+          <Typography variant="subtitle1" component="div">
+            Moznost vratenia stanu do 30 dni
+          </Typography>
+        </OptionContent>
+      </Option>
+
+      <Divider orientation="vertical" flexItem />
+
+      <Option>
+        <IconBox>
+          <LocalShippingIcon />
+        </IconBox>
+        <OptionContent>
+          <Typography variant="h5" fontWeight="500" component="div">
+            Doprava zdarma
+          </Typography>
+          <Typography variant="subtitle1" component="div">
+            Do 2km od Petrzalky
+          </Typography>
+        </OptionContent>
+      </Option>
+
+      <Divider orientation="vertical" flexItem />
+
+      <Option>
+        <IconBox>
+          <QuickreplyIcon />
+        </IconBox>
+        <OptionContent>
+          <Typography variant="h5" fontWeight="500" component="div">
+            Osobne prebratie
+          </Typography>
+          <Typography variant="subtitle1" component="div">
+            S moznostou instruktaze na Drazdiaku
+          </Typography>
+        </OptionContent>
+      </Option>
+    </StyledBox>
+  );
 }
