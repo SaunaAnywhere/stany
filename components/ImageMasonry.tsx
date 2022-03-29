6947ad7296b3752b1e5793cd760287f2240effb1
Matej Sajgal <Matej_Sajgal@swissre.com>
2022-03-29 20:56:04 +0200

    Adds SingleImage and ImageMasonry components

--- components/ImageMasonry.tsx
+++ components/ImageMasonry.tsx
@@ -1,5 +1,52 @@
-import { Box } from "@mui/material";
+import { styled } from "@mui/system";
+import { Box, Container, Grid } from "@mui/material";
+import Image from "next/image";
+
+const StyledContainer = styled(Grid)(({ theme }) => ({
+  height: "10em",
+  position: "relative",
+
+  [theme.breakpoints.up("sm")]: {
+    height: "20em",
+  },
+
+  [theme.breakpoints.up("md")]: {
+    height: "30em",
+  },
+
+  [theme.breakpoints.up("lg")]: {
+    height: "50em",
+  },
+}));
+
+const ImageTile = styled(Grid)`
+  position: relative;
+  overflow: hidden;
+`;
+
+const ImageList = [
+  "/images/slider/city.jpeg",
+  "/images/slider/river.jpeg",
+  "/images/slider/winter.jpeg",
+  "/images/slider/winter.jpeg",
+  "/images/slider/city.jpeg",
+  "/images/slider/river.jpeg",
+];
 
 export default function ImageMasonry() {
-  return <Box>Image masonry</Box>;
+  return (
+    <StyledContainer container>
+      {ImageList.map((image, index) => (
+        <ImageTile item xs={4} key={index}>
+          <Image
+            alt="image"
+            src={image}
+            layout="fill"
+            objectFit="cover"
+            priority={true}
+          />
+        </ImageTile>
+      ))}
+    </StyledContainer>
+  );
 }
--- components/SingleImage.tsx
+++ components/SingleImage.tsx
@@ -1,5 +1,34 @@
-import { Box } from "@mui/material";
+import { styled } from "@mui/system";
+import { Box, Container } from "@mui/material";
+import Image from "next/image";
+
+const StyledContainer = styled(Container)(({ theme }) => ({
+  height: "10em",
+  position: "relative",
+
+  [theme.breakpoints.up("sm")]: {
+    height: "20em",
+  },
+
+  [theme.breakpoints.up("md")]: {
+    height: "30em",
+  },
+
+  [theme.breakpoints.up("lg")]: {
+    height: "50em",
+  },
+}));
 
 export default function SingleImage() {
-  return <Box>Single Wide Image</Box>;
+  return (
+    <StyledContainer maxWidth={false}>
+      <Image
+        alt="image"
+        src="/images/slider/winter.jpeg"
+        layout="fill"
+        objectFit="cover"
+        priority={true}
+      />
+    </StyledContainer>
+  );
 }
